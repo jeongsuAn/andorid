008e217b85c86e479ef36b6b98ae9721dc3b2877
jeongsu <jeongsu@jeongsuui-MacBookPro.local>
2021-01-29 23:21:19 +0900

    Initial commit

---
+++ app/src/main/java/kotlin/01.Variable.kt
@@ -0,0 +1,21 @@
+package com.example.fastcampus.kotlin
+
+//변수
+//  - variable : 변할 수 있음
+//  - value : 변할 수 없음
+//변수를 선언하는 방법
+// var/val 변수명 = 값
+
+var num = 10
+var hello = "hello"
+var point = 3.4
+
+val fix = 20
+
+fun main(arg:Array<String>){
+
+    println(num)
+    println(hello)
+    println(point)
+    println(fix)
+}
