b8057bf100670eadb248a1903dcde8684eb5f31d
jeongsuAn <dks9467@naver.com>
2021-02-04 23:41:23 +0900

    practice android

--- app/src/main/java/com/example/fastcampus/kotlin/16.Iterable.kt
+++ app/src/main/java/com/example/fastcampus/kotlin/16.Iterable.kt
@@ -32,13 +32,13 @@ fun main(array: Array<String>){
         println(item)
     }
     println()
-    //반복하는 방법 (5)
+    //반복하는 방법 (5)  //이게 좀 더 직관적이고 좋다
     a.forEachIndexed{ index, item -> //index는 그대로 써줘야하는데 item부분은 바꿔줘도 무방
         println("index : " + index + "value : "+ item)
     }
 
-    //반복하는 방법 (6)
-    for(i in 0 until 9){
+    //반복하는 방법 (6) 인덱스만 순회하는 for문
+    for(i in 0 until a.size){
         //until은 마지막을 포함하지 않는다.
         //0부터 8까지 이다.
         println(i)
@@ -47,8 +47,41 @@ fun main(array: Array<String>){
 
     println("a. size = "+a.size)
     //반복하는 방법 (7)
-    for(i in 0 until a.size step (2)) //
+    for(i in 0 until a.size step (2)) { //
+        println(i)
+        println(a.get(i))
+    }
+    println()
+
+    //반복하는 방법 (8)
+    for(i in a.size - 1 downTo (0)){
+        // a.size는 9  // 8부터 0까지 반복
+        println(a.get(i))
+    }
+    println()
+    //반복하는 방법 (9)
+    for(i in a.size - 1 downTo (0) step(2))
+        println(a.get(i))
+
+    //반복하는 방법 (10)
+    for (i in 0 .. a.size) //until이면 마지막 제외인데 ..은 마지막 포함
         println(i)
 
+    //반복하는 방법 (11)
+    var b: Int = 0
+    var c: Int = 4
+
+    while (b<c){
+        b++
+        println("b : " + b )
+    }
+    // 반복하는 방법 (12)
+    println()
+    var d: Int = 0
+    var e: Int = 4
+    do{ //do 는 조건을 실행시키지 못하더라도 무조건 한번을 실행된다.
+        println("hello")
+        d++
+    }while (d<e)
 
 }
