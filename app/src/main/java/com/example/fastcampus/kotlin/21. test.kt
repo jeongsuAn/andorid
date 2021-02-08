014596a5166ff88712e1bdfb44d3c9d6c31c33ef
jeongsuAn <dks9467@naver.com>
2021-02-08 23:39:09 +0900

    practice android

---
+++ app/src/main/java/com/example/fastcampus/kotlin/21. test.kt
@@ -0,0 +1,21 @@
+package com.example.fastcampus.kotlin
+
+fun main(array: Array<String>){
+
+
+
+}
+
+class TVchanel(){
+    var chanel1:String = "MBC"
+    var chanel2:String = "SBS"
+    var chanel3:String = "KBS"
+    var TV:Boolean = false
+    fun tvOnOff(){
+        TV = !TV
+    }
+    fun chanelUP(){
+
+    }
+
+}
