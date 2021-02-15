82f8708dd7d39a11707f1fae40104fbbf71958be
jeongsuAn <dks9467@naver.com>
2021-02-15 23:55:16 +0900

    practice android

---
+++ app/src/main/java/com/example/fastcampus/Linstener.kt
@@ -0,0 +1,22 @@
+package com.example.fastcampus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+
+class Linstener : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linstener)
+
+        //뷰를 activity로 가져오는 방법
+        //1> 직접 찾아서 가져온다
+//        val textView: TextView = findViewById(R.id.helloButton)
+        //2> xml을 import해서 가져온다
+//        hellobutton
+
+
+
+    }
+}
