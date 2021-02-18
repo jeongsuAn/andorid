5a9d9ea4907e796412cfefaaa17f69133f1a5f83
jeongsuAn <dks9467@naver.com>
2021-02-18 23:30:25 +0900

    practice android

--- app/src/main/java/com/example/fastcampus/Calculator.kt
+++ app/src/main/java/com/example/fastcampus/Calculator.kt
@@ -13,6 +13,8 @@ class Calculator : AppCompatActivity() {
         var cal_operand = 0
         var cal_operator = ""
         var cal_result = 0
+        var cal_floatResult = 0.0
+        val Cal_result: TextView = findViewById(R.id.cal_result)
         val Cal_0: TextView = findViewById(R.id.cal_0)
         val Cal_1: TextView = findViewById(R.id.cal_1)
         val Cal_2: TextView = findViewById(R.id.cal_2)
@@ -33,80 +35,149 @@ class Calculator : AppCompatActivity() {
         val Cal_div: TextView = findViewById(R.id.cal_div)
 
 
-        Cal_0.setOnClickListener{
-            calNumber += "0"
+        Cal_0.setOnClickListener {
+            if (calNumber == "0") {
+                calNumber = "0"
+            } else {
+                calNumber += "0"
+            }
+            Cal_result.setText(calNumber)
         }
-        Cal_1.setOnClickListener{
-            calNumber += "1"
+        Cal_1.setOnClickListener {
+            if (calNumber == "0") {
+                calNumber = "1"
+            } else {
+                calNumber += "1"
+            }
+            Cal_result.setText(calNumber)
         }
-        Cal_2.setOnClickListener{
-            calNumber += "2"
+        Cal_2.setOnClickListener {
+            if (calNumber == "0") {
+                calNumber = "2"
+            } else {
+                calNumber += "2"
+            }
+            Cal_result.setText(calNumber)
         }
-        Cal_3.setOnClickListener{
-            calNumber += "3"
+        Cal_3.setOnClickListener {
+            if (calNumber == "0") {
+                calNumber = "3"
+            } else {
+                calNumber += "3"
+            }
+            Cal_result.setText(calNumber)
         }
-        Cal_4.setOnClickListener{
-            calNumber += "4"
+        Cal_4.setOnClickListener {
+            if (calNumber == "0") {
+                calNumber = "4"
+            } else {
+                calNumber += "4"
+            }
+            Cal_result.setText(calNumber)
         }
-        Cal_5.setOnClickListener{
-            calNumber += "5"
+        Cal_5.setOnClickListener {
+            if (calNumber == "0") {
+                calNumber = "5"
+            } else {
+                calNumber += "5"
+            }
+            Cal_result.setText(calNumber)
         }
-        Cal_6.setOnClickListener{
-            calNumber += "6"
+        Cal_6.setOnClickListener {
+            if (calNumber == "0") {
+                calNumber = "6"
+            } else {
+                calNumber += "6"
+            }
+            Cal_result.setText(calNumber)
         }
-        Cal_7.setOnClickListener{
-            calNumber += "7"
+        Cal_7.setOnClickListener {
+            if (calNumber == "0") {
+                calNumber = "7"
+            } else {
+                calNumber += "7"
+            }
+            Cal_result.setText(calNumber)
         }
-        Cal_8.setOnClickListener{
-            calNumber += "8"
+        Cal_8.setOnClickListener {
+            if (calNumber == "0") {
+                calNumber = "8"
+            } else {
+                calNumber += "8"
+            }
+            Cal_result.setText(calNumber)
         }
-        Cal_9.setOnClickListener{
-            calNumber += "9"
+        Cal_9.setOnClickListener {
+            if (calNumber == "0") {
+                calNumber = "9"
+            } else {
+                calNumber += "9"
+            }
+            Cal_result.setText(calNumber)
         }
         Cal_plus.setOnClickListener {
             cal_operand = calNumber.toInt()
             cal_operator = "+"
+            Cal_result.setText(calNumber)
             calNumber = ""
         }
         Cal_minus.setOnClickListener {
             cal_operand = calNumber.toInt()
             cal_operator = "-"
+            Cal_result.setText(calNumber)
             calNumber = ""
         }
         Cal_mul.setOnClickListener {
             cal_operand = calNumber.toInt()
             cal_operator = "*"
+            Cal_result.setText(calNumber)
             calNumber = ""
         }
         Cal_div.setOnClickListener {
             cal_operand = calNumber.toInt()
             cal_operator = "/"
+            Cal_result.setText(calNumber)
             calNumber = ""
         }
         Cal_percent.setOnClickListener {
             cal_operand = calNumber.toInt()
-            cal_result = cal_operand/100
+            cal_floatResult = (cal_operand.toDouble() / 100.0)
+            calNumber = "" + cal_floatResult
+            Cal_result.setText(calNumber)
         }
         Cal_sign.setOnClickListener {
-            if(calNumber.toInt()>0) {
+            if (calNumber.toInt() > 0) {
                 calNumber = "-" + calNumber
+            }else{
+                calNumber = ((-1) * calNumber.toInt()).toString()
             }
-            if(calNumber.toInt()<0) {
-                cal_operator = ((-1)*calNumber.toInt()).toString()
-            }
+            Cal_result.setText(calNumber)
         }
-
+        Cal_C.setOnClickListener {
+            calNumber = "0"
+            Cal_result.setText(calNumber)
+        }
         Cal_equal.setOnClickListener {
-            when(cal_operator){
-                "+" -> cal_result = cal_operand + calNumber.toInt()
-                "-" -> cal_result = cal_operand - calNumber.toInt()
-                "*" -> cal_result = cal_operand * calNumber.toInt()
-                "/" -> cal_result = cal_operand / calNumber.toInt()
+            when (cal_operator) {
+                "+" -> {
+                    cal_result = cal_operand + calNumber.toInt()
+                    calNumber = "" + cal_result
+                }
+                "-" -> {
+                    cal_result = cal_operand - calNumber.toInt()
+                    calNumber = "" + cal_result
+                }
+                "*" -> {
+                    cal_result = cal_operand * calNumber.toInt()
+                    calNumber = "" + cal_result
+                }
+                "/" -> {cal_floatResult = cal_operand.toDouble() / calNumber.toDouble()
+                    calNumber = "" + cal_floatResult
+                }
             }
-            calNumber = ""+cal_result
+
+            Cal_result.setText(calNumber)
         }
-
-
 
 
     }
