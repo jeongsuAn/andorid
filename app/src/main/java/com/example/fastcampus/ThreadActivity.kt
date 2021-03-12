6d24be0d0f5e3d2764867d9c09c6b550bb4dd606
jeongsuAn <dks9467@naver.com>
2021-03-12 23:55:56 +0900

    practice android

--- app/src/main/java/com/example/fastcampus/ThreadActivity.kt
+++ app/src/main/java/com/example/fastcampus/ThreadActivity.kt
@@ -2,10 +2,20 @@ package com.example.fastcampus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class ThreadActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_thread)
+
+
+        val thread: Thread = Thread()
+
+        val runnable: Runnable = object  : Runnable{
+            override fun run() {
+                Log.d("thread-1","Thread is made")
+            }
+        }
     }
 }
